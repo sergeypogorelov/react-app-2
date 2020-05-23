d49687db071b0031764e40daf3145f2fe849df86
Sergey Pogorelov <sergey.pogorelov.93@gmail.com>
2020-05-23 16:15:04 +0400

    Refactor components on the view movie page

--- src/client/app/shared/movies-list/movie-item/movie-item.component.tsx
+++ src/client/app/shared/movies-list/movie-item/movie-item.component.tsx
@@ -2,13 +2,13 @@ import './movie-item.component.scss';
 
 import React, { FunctionComponent } from 'react';
 
-const killBillModule = require('../../../../assets/images/kill-bill.png');
+import { imageKillBill } from '../../../constants/assets-urls';
 
 export const MovieItem: FunctionComponent<{}> = () => {
   return (
     <div className="movie-item">
-      <img className="movie-item-poster" src={killBillModule.default} />
-      <div className="movie-item-top d-flex justify-content-between align-items-center">
+      <img className="movie-item-poster" src={imageKillBill} />
+      <div className="movie-item-top">
         <h3 className="movie-item-title mb-0">Kill Bill: Vol 2</h3>
         <label className="movie-item-year mb-0">1994</label>
       </div>
