87fde6d736c92a57b3a69e9f611a894f362017d5
Sergey Pogorelov <sergey.pogorelov.93@gmail.com>
2020-06-15 14:53:29 +0400

    Get rid of redux-persist

--- src/client/app/app.component.tsx
+++ src/client/app/app.component.tsx
@@ -6,7 +6,6 @@ import {
   Redirect,
 } from 'react-router-dom';
 import { Provider as ReduxProvider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
 
 import { urlFragments } from './core/constants/url-fragments';
 
@@ -18,36 +17,34 @@ import { SearchMoviePage } from './pages/search-movie/search-movie-page.componen
 import { ViewMoviePage } from './pages/view-movie/view-movie-page.component';
 import { NotFoundPage } from './pages/not-found/not-found-page.component';
 
-const { store, persistor } = configureStore(initialState);
+const store = configureStore(initialState);
 
 export const App = () => {
   return (
     <ReduxProvider store={store}>
-      <PersistGate persistor={persistor}>
-        <Router>
-          <Page>
-            <Switch>
-              <Route path="/" exact>
-                <Redirect to={`/${urlFragments.searchMovie}`} />
-              </Route>
-              <Route
-                path={`/${urlFragments.searchMovie}/:search`}
-                component={SearchMoviePage}
-              />
-              <Route path={`/${urlFragments.searchMovie}`}>
-                <SearchMoviePage />
-              </Route>
-              <Route
-                path={`/${urlFragments.viewMovie}/:movieId`}
-                component={ViewMoviePage}
-              />
-              <Route path="**">
-                <NotFoundPage />
-              </Route>
-            </Switch>
-          </Page>
-        </Router>
-      </PersistGate>
+      <Router>
+        <Page>
+          <Switch>
+            <Route path="/" exact>
+              <Redirect to={`/${urlFragments.searchMovie}`} />
+            </Route>
+            <Route
+              path={`/${urlFragments.searchMovie}/:search`}
+              component={SearchMoviePage}
+            />
+            <Route path={`/${urlFragments.searchMovie}`}>
+              <SearchMoviePage />
+            </Route>
+            <Route
+              path={`/${urlFragments.viewMovie}/:movieId`}
+              component={ViewMoviePage}
+            />
+            <Route path="**">
+              <NotFoundPage />
+            </Route>
+          </Switch>
+        </Page>
+      </Router>
     </ReduxProvider>
   );
 };
